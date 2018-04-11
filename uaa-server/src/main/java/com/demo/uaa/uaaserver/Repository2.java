d30d0d23b10ffbc6e51b780024fd4c31f5503365
Girish-Mehta <girish.mehta100@gmail.com>
2018-04-11 12:40:34 +0530

    Added files

---
+++ uaa-server/src/main/java/com/demo/uaa/uaaserver/Repository2.java
@@ -0,0 +1,7 @@
+package com.demo.uaa.uaaserver;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface Repository2 extends MongoRepository<DetailsModal, String> {
+
+}
